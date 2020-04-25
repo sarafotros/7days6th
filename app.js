057d6e5075fd7248df9ad81ddb7c447526dcfdb6
sarafotros <sarafotros@gmail.com>
2020-04-26 00:34:49 +0100

    done

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = () => {
 
     const sounds = document.querySelectorAll('.sound-picker button');
     const timeDisplay = document.querySelector('.time-display');
-    
+    const timeSelect = document.querySelectorAll('.time-select button');
     const outlineLength = outline.getTotalLength();
   
     let fakeDuration = 600;
@@ -14,10 +14,63 @@ const app = () => {
     outline.style.strokeDasharray = outlineLength;
     outline.style.strokeDashoffset = outlineLength;
 
+
+    //pick different sounds
+    sounds.forEach(sound => {
+        sound.addEventListener('click', function () {
+            song.src = this.getAttribute('data-sounds')
+            video.src = this.getAttribute('data-video') 
+            checkPlaying(song)
+        })
+    })
+
+
     //play sound
     play.addEventListener('click', () => {
-        song.play();
+        checkPlaying(song);
     })
+
+
+    timeSelect.forEach(option => {
+        option.addEventListener('click', function () {
+            fakeDuration = this.getAttribute('data-time');
+            timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}: ${Math.floor(fakeDuration % 60)}`
+        })
+    }) 
+
+
+    //stop and play 
+    const checkPlaying = song => {
+        if (song.paused) {
+            song.play();
+            video.play();
+            play.src = './svg/pause.svg';
+        } else {
+            song.pause();
+            video.pause();
+            play.src = './svg/play.svg'
+        }
+    };
+
+    //animate song ontimeupdate
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+
+        timeDisplay.textContent = `${minutes}:${seconds}`
+
+        if (currentTime >= fakeDuration) {
+            song.pause();
+            song.currentTime = 0;
+            play.src = './svg/play.svg';
+            video.pause();
+        }
+    }
 
 }
 app();
